1b06a21fa26f9db160bfbb6f614f6bcce3e6714c
Przemek <sniffoza@gmail.com>
2018-02-12 22:10:53 +0100

    Add registration spec for already taken email.

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User registrations' do
+  before(:all) do
+    @user = FactoryBot.create(:user)
+  end
+
   let(:user) { FactoryBot.attributes_for(:user).to_json }
   let(:user_without_email) do
     FactoryBot.attributes_for(:user, :without_email).to_json
   end
   let(:user_without_password_confirmation) do
     FactoryBot.attributes_for(:user, :without_password_confirmation).to_json
+  end
+  let(:user_with_duplicate_email) do
+    FactoryBot.attributes_for(:user, email: @user.email).to_json
   end
 
   context 'Registrations#create POST' do
@@ -24,12 +31,21 @@ RSpec.describe 'User registrations' do
       expect(response).to have_http_status(422)
     end
 
-    it 'doesnt create new user without password confirmation' do
+    it 'doesnt create new user without password confirmation, responds 422' do
       expect {
         post user_registration_path,
              params: user_without_password_confirmation, headers: headers
       }.not_to(change { User.count })
       expect(response).to have_http_status(422)
+    end
+
+    it 'doesnt create new user if email is already in use, responds 422' do
+      expect {
+        post user_registration_path,
+             params: user_with_duplicate_email, headers: headers
+      }.not_to(change { User.count })
+      expect(response).to have_http_status(422)
+      expect(json[:errors]).to include email: ['has already been taken']
     end
   end
 end
