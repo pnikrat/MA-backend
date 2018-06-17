48e26442dbba38586b405b3a749edb117128255f
Przemek <sniffoza@gmail.com>
2018-06-17 18:21:51 +0200

    Has many through relationships model tests part 2

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,13 +7,4 @@ class Group < ApplicationRecord
   has_many :group_memberships, dependent: :destroy
   has_many :users, through: :group_memberships
   belongs_to :creator, class_name: 'User'
-
-  # before_save :assign_creator_to_users, on: :create
-
-  # private
-
-  # def assign_creator_to_users
-  #   binding.pry
-  #   users << creator unless users.include? creator
-  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe User do
   let(:user_without_email) { build(:user, :without_email) }
   let(:user_without_first_name) { build(:user, :without_first_name) }
   let(:user_with_groups) { create(:user, :with_groups) }
+  let(:group) { create(:group) }
+  let(:group_attr) { attributes_for(:group, creator_id: user_with_groups.id) }
 
   context 'basic model validations' do
     it 'user with email, password and first_name is valid' do
@@ -28,6 +30,33 @@ RSpec.describe User do
   context 'has many through associations' do
     it 'user can create many groups' do
       expect(user_with_groups.groups.length).to eq 3
+    end
+
+    it 'user with groups creates necessary group memberships' do
+      expect {
+        user_with_groups
+      }.to change(GroupMembership, :count).by 3
+    end
+
+    it 'user with groups can get new existing group' do
+      user_with_groups.groups << group
+      expect(user_with_groups.groups.length).to eq 4
+      expect(GroupMembership.count).to eq 4
+    end
+
+    it 'user with groups can create new group' do
+      user_with_groups.groups.create(group_attr)
+      expect(user_with_groups.groups.length).to eq 4
+      expect(GroupMembership.count).to eq 4
+    end
+
+    it 'does not accept the same group into collection' do
+      existing_group = user_with_groups.groups.first
+      expect {
+        user_with_groups.groups << existing_group
+      }.to raise_error ActiveRecord::RecordInvalid
+      expect(GroupMembership.count).to eq 3
+      expect(user_with_groups.groups.reload.length).to eq 3
     end
   end
 end
