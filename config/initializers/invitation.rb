9e1dd7bd09ccf589ec51e6bc7c43db09b381acff
Przemek <sniffoza@gmail.com>
2018-07-12 20:16:24 +0200

    Additional gem config, set email views

--- config/initializers/invitation.rb
+++ config/initializers/invitation.rb
@@ -1,7 +1,13 @@
 Invitation.configure do |config|
   # config.user_model = '::User'
-  # config.user_registration_url = ->(params) { Rails.application.routes.url_helpers.sign_up_url(params) }
-  # config.mailer_sender = 'reply@example.com'
+  config.user_registration_url = lambda do |params|
+    url = ENV['FRONT_REGISTRATION_URL'] + '?'
+    params.each do |k, v|
+      url += "#{k}=#{v}&"
+    end
+    url[0...-1]
+  end
+  config.mailer_sender = ENV['MAILER_SENDER']
   config.routes = false
   # config.case_sensitive_email = true
 end
