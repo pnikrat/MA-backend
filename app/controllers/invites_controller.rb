24853c210505fad0fdf1212251b3aa49d1cf45e4
Przemek <sniffoza@gmail.com>
2018-07-12 20:16:45 +0200

    Finish invite creation spec

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,6 +2,7 @@
 class InvitesController < Invitation::InvitesController
   before_action :authenticate_user!
   before_action :authorize
+  before_action :check_if_permissions_already_exist
 
   include JSONErrors
   include ActionController::MimeResponds
@@ -15,5 +16,15 @@ class InvitesController < Invitation::InvitesController
 
   def load_invitable
     invite_params[:invitable_type].classify.constantize.find(invite_params[:invitable_id])
+  end
+
+  def check_if_permissions_already_exist
+    return unless user_already_in_group
+    render json: custom_error('User already in group'), status: :bad_request
+  end
+
+  def user_already_in_group
+    GroupMembership.joins(:user).
+      where(users: { email: invite_params[:email] }, group_id: invite_params[:invitable_id]).exists?
   end
 end
--- spec/requests/invites_spec.rb
+++ spec/requests/invites_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Group invitation creation' do
   let(:group) { user.groups.first }
   let(:user2) { create(:user) }
   let(:invite_params) { attributes_for(:invite, invitable_id: group.id) }
+  let(:existing_invite_params) { invite_params.merge(email: user2.email) }
+  let(:invalid_invite_params) { attributes_for(:invite, :invalid, invitable_id: group.id) }
+  let(:deliveries) { ActionMailer::Base.deliveries }
 
   context 'Invites#create POST' do
     it 'Raises proper error when user is not authenticated' do
@@ -23,7 +26,7 @@ RSpec.describe 'Group invitation creation' do
       expect(json[:errors]).to eq 'unauthorized access'
     end
 
-    it 'creates an invite when user is authenticated and authorized to sending invites' do
+    it 'creates invite for new user when inviter is authenticated and authorized to invite' do
       expect {
         post invites_path, params: invite_params.to_json, headers: headers(user)
       }.to change(Invite, :count).by 1
@@ -31,6 +34,42 @@ RSpec.describe 'Group invitation creation' do
       expect(json[:email]).to eq invite_params[:email]
       expect(json[:invitable_id]).to eq group.id
       expect(json[:sender_id]).to eq user.id
+      expect(deliveries.length).to eq 1
+      expect(deliveries.first.from).to eq [ENV['MAILER_SENDER']]
+      expect(deliveries.first.to).to eq [invite_params[:email]]
+      expect(deliveries.first.to_s).to include(
+        I18n.t('invitation.invite_mailer.new_user.someone_invited_you',
+               sender: user.email, invitable: group.name)
+      )
+    end
+
+    it 'creates invite for existing user and instantly gives access to existing user' do
+      expect(Group.with_member(user2)).not_to include group
+      expect {
+        post invites_path, params: existing_invite_params.to_json, headers: headers(user)
+      }.to change(Invite, :count).by 1
+      expect(response).to have_http_status :created
+      expect(json[:email]).to eq user2.email
+      expect(json[:sender_id]).to eq user.id
+      expect(deliveries.length).to eq 1
+      expect(deliveries.first.to).to eq [user2.email]
+      expect(deliveries.first.to_s).to include(
+        I18n.t('invitation.invite_mailer.existing_user.someone_invited_you',
+               sender: user.email, invitable: group.name)
+      )
+      expect(Group.with_member(user2)).to include group
+    end
+
+    it 'doesnt create invite for user who already has access to invitable. No email sent' do
+      user2.groups << group
+      expect(Group.with_member(user2)).to include group
+      expect {
+        post invites_path, params: existing_invite_params.to_json, headers: headers(user)
+      }.not_to change(Invite, :count)
+      expect(response).to have_http_status :bad_request
+      expect(json[:status]).to eq 'failed'
+      expect(json[:errors]).to eq 'User already in group'
+      expect(deliveries.length).to eq 0
     end
   end
 end
