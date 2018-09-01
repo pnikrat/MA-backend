fda6308aaafbf2c95ed7353570eb2fe93569a7a7
Przemek <sniffoza@gmail.com>
2018-09-01 23:57:10 +0200

    Tweak settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://glacial-river-38509.herokuapp.com'
+  host = 'glacial-river-38509.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
